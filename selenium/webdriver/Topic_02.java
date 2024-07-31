522721095bd57f3a2263970d9a0677124b601011
Joshua Nguyen <minhquanlk1607@gmail.com>
2024-07-31 21:46:42 +0700

    edit url in glab.java

--- selenium/webdriver/Topic_02.java
+++ selenium/webdriver/Topic_02.java
@@ -1,5 +1,9 @@
 package webdriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Topic_02 {
     //add new
+
 }
--- selenium/webdriver/glab.java
+++ selenium/webdriver/glab.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
@@ -11,24 +13,10 @@ public class glab {
 
     WebDriver driver;
 
-    //url
-    String url = "https://glab-v2.khgc.dev/login";
-
-    //String input
-    String userName = "luffymurom@gmail.com";
-//    String password = "Quan@123";
-    //element
-    WebElement txtUsername = driver.findElement(By.xpath("//div//input[@name='email']"));
-
-    Actions action = new Actions(driver);
-
     @Test
     public void TC_01_Glab() {
         driver = new ChromeDriver();
-        driver.get(url);
-        action.sendKeys(txtUsername, userName);
+        driver.get("https://glab-v2.khgc.dev/login");
         driver.quit();
     }
-
-
 }
