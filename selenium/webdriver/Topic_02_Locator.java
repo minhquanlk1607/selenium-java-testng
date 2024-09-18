847fcebabf9592918fd45f7a19a48f548ab9f552
Joshua Nguyen <minhquanlk1607@gmail.com>
2024-09-18 22:34:55 +0700

    Topic DataType và Primitive Reference

--- selenium/webdriver/Topic_02_Locator.java
+++ selenium/webdriver/Topic_02_Locator.java
@@ -2,7 +2,10 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,37 +15,54 @@ import java.time.Duration;
 public class Topic_02_Locator {
 
     WebDriver driver;
+    WebDriverWait wait;
+
+    private static final String URL = "https://glab-v2.khgc.dev/register";
+    private static final String TXT_FIRST_NAME = "//input[@id='firstName']";
+    private static final String TXT_LAST_NAME = "//input[@id='lastName']";
+    private static final String TXT_EMAIL = "//input[@id='emailInput']";
+    private static final String TXT_PASSWORD = "//input[@id='passwordInput']";
+    private static final String TXT_CONFIRM_PASSWORD = "//input[@id='passwordCfmInput']";
+    private static final String CHECKBOX_AGREE = "//input[@id='agreeCkb']";
+    private static final String BTN_REGISTER = "(//button[@type='submit'])[2]";
 
     @BeforeClass
-    public void beforeclass(){
+    public void beforeClass() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));  // Explicit wait initialization
     }
+
+    private void enterText(String locator, String value) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    private void clickElement(String locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+        element.click();
+    }
+
     @Test
     public void TC_01_GotoLinkGoogle() {
+        driver.get(URL);
 
-        String txtFirstName = "//input[@id='firstName']";
-        String txtLastName = "//input[@id='lastName']";
-        String url = "https://glab-v2.khgc.dev/register";
-        String txtEmail = "//input[@id='emailInput']";
-        String txtPass = "//input[@id='passwordInput']";
-        String txtConfirmPass = "//input[@id='passwordCfmInput']";
-        String checkboxAgree = "//input[@id='agreeCkb']";
-        String btnRegister = "(//button[@type='submit'])[2]";
+        // Enter form data
+        enterText(TXT_FIRST_NAME, "First Name");
+        enterText(TXT_LAST_NAME, "Last Name");
+        enterText(TXT_EMAIL, "abc1@gmail.com");
+        enterText(TXT_PASSWORD, "Test@123123");
+        enterText(TXT_CONFIRM_PASSWORD, "Test@123123");
 
-        driver.get(url);
-        driver.findElement(By.xpath(txtFirstName)).sendKeys("First Name");
-        driver.findElement(By.xpath(txtLastName)).sendKeys("Last Name");
-        driver.findElement(By.xpath(txtEmail)).sendKeys("abc@gmail.com");
-        driver.findElement(By.xpath(txtPass)).sendKeys("Test@123123");
-        driver.findElement(By.xpath(txtConfirmPass)).sendKeys("Test@123123");
-        driver.findElement(By.xpath(checkboxAgree)).click();
-        driver.findElement(By.xpath(btnRegister)).click();
+        clickElement(CHECKBOX_AGREE);
+        clickElement(BTN_REGISTER);
     }
 
     @AfterClass
-    public void afterclass(){
-    //    driver.quit();
+    public void afterClass() {
+        if (driver != null) {
+            driver.quit();  // Ensure proper teardown
+        }
     }
-
 }
